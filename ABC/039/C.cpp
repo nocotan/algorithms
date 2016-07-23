9bfcf6171a36f926a5461b12da93c80f0c48cdb9
nocotan <noconoco.lib@gmail.com>
2016-07-23 16:15:27 +0900

    ABC

---
+++ ABC/039/C.cpp
@@ -0,0 +1,102 @@
+//#define _GRIBCXX_DEBUG
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+
+// 基本テンプレート
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define repi(i,n) for(int i=0; i<=(int)n; ++i)
+#define REPI(i,x,n) for(int i=x; i<=(int)n; ++i)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), (c).end()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<double> vd;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<long, long> pil;
+
+
+// Effective std
+template<typename C, typename T>
+constexpr int count(C& c, T t) { return count(ALL(c), t); }
+
+template<typename C, typename F>
+constexpr int count_if(C& c, F f) { return count_if(ALL(c), f); }
+
+template<typename C, typename T, typename U>
+constexpr void replace(C& c, T t, U u) { replace(ALL(c), t, u); }
+
+template<typename C, typename F, typename U>
+constexpr void replace_if(C& c, F f, U u) { (ALL(c), f, u); }
+
+template<typename C>
+constexpr void sort(C& c) { sort(ALL(c)); }
+
+template<typename C, typename Pred>
+constexpr void sort(C& c, Pred p) { sort(ALL(c), p); }
+
+
+// グラフテンプレート
+typedef int Weight;
+
+struct Edge {
+  int src, dst;
+  Weight weight;
+  Edge(int src, int dst, Weight weight) :
+    src(src), dst(dst), weight(weight) {}
+};
+
+bool operator < (const Edge &e, const Edge &f) {
+  return e.weight != f.weight ? e.weight > f.weight :
+    e.src != f.src ? e.src < f.src : e.dst < f.dst;
+}
+
+typedef vector<Edge> Edges;
+typedef vector<Edges> Graph;
+typedef vector<Weight> Array;
+typedef vector<Array> Matrix;
+
+int main() {
+  string s;
+  cin >> s;
+    cin >> s;
+  s.resize(12);
+  if (s == "WBWBWWBWBWBW")
+    cout << "Do" << endl;
+  else if (s == "WBWWBWBWBWWB")
+    cout << "Re" << endl;
+  else if (s == "WWBWBWBWWBWB")
+    cout << "Mi" << endl;
+  else if (s == "WBWBWBWWBWBW")
+    cout << "Fa" << endl;
+  else if (s == "WBWBWWBWBWWB")
+    cout << "So" << endl;
+  else if (s == "WBWWBWBWWBWB")
+    cout << "La" << endl;
+  else if (s == "WWBWBWWBWBWB")
+    cout << "Si" << endl;
+  return 0;
+}

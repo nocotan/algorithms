85878a3b6c79ed7fc96678459abcd72175a16097
nocotan <noconoco.lib@gmail.com>
2016-12-10 22:40:30 +0900

    ABC

---
+++ Cpp/ABC/049/D.cpp
@@ -0,0 +1,124 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+// 基本テンプレート
+#pragma region MACRO
+#define P(x) cout << (x) << endl
+#define p(x) cout << (x)
+#define PED cout << "\n"
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define repi(i,n) for(int i=0; i<=(int)n; ++i)
+#define REPI(i,x,n) for(int i=x; i<=(int)n; ++i)
+#define ILP while(true)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define mp make_pair
+#pragma endregion
+
+// 型
+#pragma region TYPE_DEF
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<long double> vld;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+#pragma endregion
+
+// Effective std
+#pragma region ESTD
+template<typename C, typename T> int count(C& c, T t) { return count(ALL(c), t); }
+template<typename C, typename F> int count_if(C& c, F f) { return count_if(ALL(c), f); }
+template<typename C, typename T> void erase(C& c, T t) { remove(ALL(c), t), c.end(); }
+template<typename C> void remove(vector<C>& c, unsigned int index) { c.erase(c.begin()+index); }
+template<typename C, typename T, typename U> void replace(C& c, T t, U u) { replace(ALL(c), t, u); }
+template<typename C, typename F, typename U> void replace_if(C& c, F f, U u) { (ALL(c), f, u); }
+template<typename C> void reverse(C& c) { reverse(ALL(c)); }
+template<typename C> void sort(C& c) { sort(ALL(c)); }
+template<typename C, typename Pred> void sort(C& c, Pred p) { sort(ALL(c), p); }
+#pragma endregion
+
+// 定数
+#pragma region CONST_VAL
+constexpr int PI = (2*acos(0.0));
+constexpr int EPS = (1e-9);
+constexpr int MOD = (int)(1e9+7);
+constexpr int INF = 100000000;
+#pragma endregion
+
+struct UnionFind {
+  vector<int> data;
+  UnionFind(int size) : data(size, -1) { }
+  bool unionSet(int x, int y) {
+    x = root(x); y = root(y);
+    if (x != y) {
+      if (data[y] < data[x]) swap(x, y);
+      data[x] += data[y]; data[y] = x;
+    }
+    return x != y;
+  }
+  bool findSet(int x, int y) {
+    return root(x) == root(y);
+  }
+  int root(int x) {
+    return data[x] < 0 ? x : data[x] = root(data[x]);
+  }
+  int size(int x) {
+    return -data[root(x)];
+  }
+};
+
+int main()
+{
+    int N, K, L;
+    cin >> N >> K >> L;
+
+    UnionFind uk(K);
+    UnionFind ul(L);
+
+    REPI(i,1,K) {
+        int p, q;
+        cin >> p >> q;
+        uk.unionSet(p,q);
+    }
+    REPI(i,1,L) {
+        int p, q;
+        cin >> p >> q;
+        ul.unionSet(p,q);
+    }
+
+    int f = 0;
+    int memo[1000000] = {1};
+    REPI(i,1,N) {
+        f = i;
+        REPI(j,i+1,N) {
+            if(uk.findSet(i,j)&&ul.findSet(i,j)&&(f+1==j||f-1==j)) {
+                memo[i]++;
+                f = j;
+            }
+        }
+        f = i;
+        for(int j=1;j<i; ++j) {
+            if(uk.findSet(i,j)&&ul.findSet(i,j)&&(f+1==j||f-1==j)) {
+                memo[i]++;
+                f = j;
+            }
+        }
+    }
+
+    rep(i,N) {
+        if(i==0) cout << memo[i];
+        else cout << " " << memo[i];
+    }
+    cout << endl;
+
+    return 0;
+}
+

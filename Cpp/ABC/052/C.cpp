d8dcca9d90bc95eb9657facb38a6bdd2e59873f6
nocotan <noconoco.lib@gmail.com>
2017-01-15 22:54:38 +0900

    ABC

---
+++ Cpp/ABC/052/C.cpp
@@ -0,0 +1,91 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+// 基本テンプレート
+#pragma region MACRO
+#define P(x) cout << (x) << endl
+#define p(x) cout << (x)
+#define PED cout << "\n"
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define repi(i,n) for(int i=0; i<=(int)n; ++i)
+#define REPI(i,x,n) for(int i=x; i<=(int)n; ++i)
+#define ILP while(true)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define mp make_pair
+#pragma endregion
+
+// 型
+#pragma region TYPE_DEF
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<long double> vld;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+#pragma endregion
+
+// Effective std
+#pragma region ESTD
+template<typename C, typename T> int count(C& c, T t) { return count(ALL(c), t); }
+template<typename C, typename F> int count_if(C& c, F f) { return count_if(ALL(c), f); }
+template<typename C, typename T> void erase(C& c, T t) { remove(ALL(c), t), c.end(); }
+template<typename C> void remove(vector<C>& c, unsigned int index) { c.erase(c.begin()+index); }
+template<typename C, typename T, typename U> void replace(C& c, T t, U u) { replace(ALL(c), t, u); }
+template<typename C, typename F, typename U> void replace_if(C& c, F f, U u) { (ALL(c), f, u); }
+template<typename C> void reverse(C& c) { reverse(ALL(c)); }
+template<typename C> void sort(C& c) { sort(ALL(c)); }
+template<typename C, typename Pred> void sort(C& c, Pred p) { sort(ALL(c), p); }
+#pragma endregion
+
+// 定数
+#pragma region CONST_VAL
+constexpr int PI = (2*acos(0.0));
+constexpr int EPS = (1e-9);
+constexpr int MOD = (int)(1e9+7);
+constexpr int INF = 100000000;
+#pragma endregion
+
+int N;
+
+vector<int> sieve_of_eratosthenes(int n) {
+  vector<int> primes(n);
+  for (int i = 2; i < n; ++i)
+    primes[i] = i;
+  for (int i = 2; i*i < n; ++i)
+    if (primes[i])
+      for (int j = i*i; j < n; j+=i)
+        primes[j] = 0;
+  return primes;
+}
+
+int main()
+{
+    cin >> N;
+    vi arr = sieve_of_eratosthenes(1000);
+    vi primes;
+    rep(i, arr.size()) {
+        if(arr[i]!=0) primes.push_back(i);
+    }
+    ll cnt = 0;
+    ll ans = 1;
+    for(int i = 0; i < primes.size(); i++){
+        int cur = primes[i];
+        int total = 0;
+        while(cur <= N){
+            total += (N/cur);
+            cur = cur*primes[i];
+        }
+        if(total!=0) ans *= (total+1);
+        ans %= MOD;
+    }
+    P(ans%MOD);
+    return 0;
+}

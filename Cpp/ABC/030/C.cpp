32b7a78f10f1e2b7ae14a643b411354f783af83c
nocotan <noconoco.lib@gmail.com>
2017-03-22 01:47:29 +0900

    ABC

--- Cpp/ABC/030/C.cpp
+++ Cpp/ABC/030/C.cpp
@@ -53,37 +53,36 @@ constexpr int MOD = (int)(1e9+7);
 constexpr int INF = 100000000;
 #pragma endregion
 
-const int nmax = 8;
-bool graph[nmax][nmax];
 
-int dfs(int v, int N, bool visited[nmax]) {
-    bool all_visited = true;
-    rep(i,N) {
-        if(visited[i]==false) all_visited = false;
-    }
-    if(all_visited) return 1;
+int main()
+{
+    int n, m, x, y;
+    cin >> n >> m >> x >> y;
+    vi a(n), b(m);
+    rep(i,n) cin >> a[i];
+    rep(i,m) cin >> b[i];
+    sort(a);
+    sort(b);
 
-    int ret = 0;
-    rep(i, N) {
-        if(graph[v][i]==false) continue;
-        if(visited[i]) continue;
-        visited[i] = true;
-        ret += dfs(i,N,visited);
-        visited[i] = false;
+    int tmp = a[0];
+    int ans = 0;
+    int dir = 1;
+    while(true) {
+        if(dir==0) {
+            auto it = lower_bound(ALL(a),tmp+y);
+            if(it==a.end()) break;
+            tmp = *it;
+            dir = 1;
+        }
+        else {
+            auto it = lower_bound(ALL(b),tmp+x);
+            if(it==b.end()) break;
+            tmp = *it;
+            ans++;
+            dir = 0;
+        }
     }
-    return ret;
+    P(ans);
+    return 0;
 }
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    rep(i,m) {
-        int a, b;
-        cin >> a >> b;
-        graph[a-1][b-1] = graph[b-1][a-1] = true;
-    }
-    bool visited[nmax];
-    rep(i,n) visited[i] = false;
-    visited[0] = true;
-    P(dfs(0,n,visited));
-}

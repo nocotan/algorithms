dda1b1b7adbbd5072cba77179b10a30ed3b9ca9b
nocotan <noconoco.lib@gmail.com>
2017-06-24 14:29:25 +0900

    arc

---
+++ Cpp/ARC/018/A.cpp
@@ -0,0 +1,13 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+auto main() -> signed {
+    double h, b;
+    cin >> h >> b;
+    cout << b * pow(h/100,2) << endl;
+    return 0;
+}
+

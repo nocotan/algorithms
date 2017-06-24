9f24aaf13e45aa5b0eca79de77cd3be93d83e426
nocotan <noconoco.lib@gmail.com>
2017-06-24 19:40:38 +0900

    arc

---
+++ Cpp/ARC/030/A.cpp
@@ -0,0 +1,14 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+auto main() -> signed {
+    int n, k;
+    cin >> n >> k;
+    if(n>=k*2) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
+

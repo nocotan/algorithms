e4a58aac25f202d4be0bc14b52d825da1ec549b9
nocotan <noconoco.lib@gmail.com>
2017-06-27 18:28:47 +0900

    arc

---
+++ Cpp/ARC/055/A.cpp
@@ -0,0 +1,15 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+auto main() -> signed {
+    int n;
+    cin >> n;
+    cout << 1;
+    for(int i=0; i<n-1; ++i) cout << 0;
+    cout << 7 << endl;
+    return 0;
+}
+

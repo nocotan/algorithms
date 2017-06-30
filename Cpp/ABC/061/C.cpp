96b87b4f788ae999d53c3e9dfdb02ac4c95f9895
nocotan <noconoco.lib@gmail.com>
2017-06-30 13:16:04 +0900

    arc

--- Cpp/ABC/061/C.cpp
+++ Cpp/ABC/061/C.cpp
@@ -4,15 +4,25 @@ using namespace std;
 
 #define int long long
 
-signed main()
-{
+int memo[100001];
+
+auto main() -> signed {
     int n, k;
     cin >> n >> k;
-    vector<int> v;
+    set<int> v;
     for(int i=0; i<n; ++i) {
         int a, b;
         cin >> a >> b;
+        v.insert(a);
+        memo[a] += b;
+    }
 
+    for(auto&& e : v) {
+        k -= memo[e];
+        if(k<=0) {
+            cout << e << endl;
+            return 0;
+        }
     }
     return 0;
 }

acab853ddb9633ea4d034d4f24b572ead28490c2
nocotan <noconoco.lib@gmail.com>
2017-08-14 21:26:56 +0900

    abc

--- Cpp/ABC/070/D.cpp
+++ Cpp/ABC/070/D.cpp
@@ -4,46 +4,67 @@ using namespace std;
 
 #define int long long
 
-int d[100000][100000];
-signed V;
+int V;
 
-void warshall_floyd() {
-    for (int k=0; k<V; k++) {
-        for (int i=0; i<V; i++) {
-            for (int j=0; j<V; j++)
-                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+#define INF LONG_MAX
+#define MAX_V 100000
+
+using P = pair<int, int>;
+
+
+struct edge {
+        int to;
+            int cost;
+};
+
+vector<edge> G[MAX_V];
+int d[MAX_V];
+
+void dijkstra(int s) {
+    priority_queue<P, vector<P>, greater<P> > que;
+    fill(d, d+V, INF);
+    d[s] = 0;
+    que.push(P(0, s));
+
+    while (!que.empty()) {
+        P p = que.top();
+        que.pop();
+        int v = p.second;
+        if (d[v] < p.first) continue;
+
+        for (int i=0; i<G[v].size(); ++i) {
+            edge e = G[v][i];
+            if (d[e.to] > d[v] + e.cost) {
+                d[e.to] = d[v] + e.cost;
+                que.push(P(d[e.to], e.to));
+            }
         }
     }
 }
 
 signed main() {
     cin >> V;
-    for(int i=0; i<V; ++i) {
-        for(int j=0; j<V; ++j) {
-            if(i==j) d[i][j] = 0;
-            else d[i][j] = LONG_MAX;
-        }
-    }
     for(int i=0; i<V-1; ++i) {
         signed a, b, c;
         cin >> a >> b >> c;
         a--;
         b--;
-        d[a][b] = c;
-        d[b][a] = c;
+        edge e = {b, c};
+        G[a].push_back(e);
+        e = {a, c};
+        G[b].push_back(e);
     }
-
-    warshall_floyd();
 
     int q, k;
     cin >> q >> k;
     k--;
+    dijkstra(k);
     int x, y;
     for(int j=0; j<q; ++j) {
         cin >> x >> y;
         x--;
         y--;
-        cout << d[x][k] + d[k][y] << endl;
+        cout << d[x] +d[y] << endl;
     }
     return 0;
 }

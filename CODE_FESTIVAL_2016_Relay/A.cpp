6ba0cdd55c458fc369b0944e4b2df04eec531c98
nocotan <noconoco.lib@gmail.com>
2017-08-25 01:32:04 +0900

    sp

---
+++ CODE_FESTIVAL_2016_Relay/A.cpp
@@ -0,0 +1,13 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    double r1, r2;
+    cin >> r1 >> r2;
+    cout << setprecision(15) << r1*r2/(r1+r2) << endl;
+    return 0;
+}
+

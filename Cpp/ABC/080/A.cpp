eb5dc9ceae75de30d063cb45770320dad9722a44
nocotan <noconoco.lib@gmail.com>
2017-12-08 16:40:25 +0900

    abc

---
+++ Cpp/ABC/080/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << min(n*a, b) << endl;
+    return 0;
+}
+

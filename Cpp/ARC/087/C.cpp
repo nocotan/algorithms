8106315e541744470be3be86f46c3292ae12e9fd
nocotan <noconoco.lib@gmail.com>
2017-12-19 18:36:48 +0900

    abc

--- Cpp/ARC/087/C.cpp
+++ Cpp/ARC/087/C.cpp
@@ -3,34 +3,20 @@ using namespace std;
 
 #define int long long
 
+int a[100000];
+
 signed main() {
     int n;
     cin >> n;
-    vector<int> v(n);
-    set<int> st;
-    for(int i=0; i<n; ++i) {
-        cin >> v[i];
-        st.insert(v[i]);
-    }
-
-    sort(v.begin(), v.end());
-
+    for(int i=0; i<n; ++i) cin >> a[i];
+    map<int, int> mp;
+    for(int i=0; i<n; ++i) mp[a[i]]++;
     int ans = 0;
-    int tmp = v[0];
-    int c = 0;
-    for(int i=0; i<n; ++i) {
-        if(tmp==v[i])
-            c++;
-        if(tmp!=v[i]||i==n-1){
-            //cout << c << " " << tmp << endl;
-            if(c >= tmp) {
-                ans += c-tmp;
-            } else {
-                ans += c;
-            }
-            c = 1;
-            tmp = v[i];
-        }
+    for(auto p : mp) {
+        int x = p.first;
+        int n = p.second;
+        if(n<x) ans += n;
+        else ans += n-x;
     }
     cout << ans << endl;
     return 0;

4c225e48e9d2a9125191333e50ee5afecdbcbb96
nocotan <noconoco.lib@gmail.com>
2018-02-24 11:36:09 +0900

    abc

---
+++ Cpp/ABC/085/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    string s;
+    cin >> s;
+    s[3] = '8';
+    cout << s << endl;
+    return 0;
+}
+

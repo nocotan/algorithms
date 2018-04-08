82980e7c696e2f58d630d60d95e0e21b78b0df8c
nocotan <noconoco.lib@gmail.com>
2018-04-08 12:34:49 +0900

    ABC

--- Cpp/ABC/088/B.cpp
+++ Cpp/ABC/088/B.cpp
@@ -4,6 +4,18 @@ using namespace std;
 #define int long long
 
 signed main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; ++i) cin >> v[i];
+    sort(v.begin(), v.end(), greater<int>());
+
+    int a=0, b=0;
+    for(int i=0; i<n; ++i) {
+        if(i%2==0) a += v[i];
+        else b += v[i];
+    }
+    cout << abs(a-b) << endl;
     return 0;
 }
 

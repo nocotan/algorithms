74483cb5e9a60b1bcde8a2fc490b178e564cacea
nocotan <noconoco.lib@gmail.com>
2018-04-08 12:19:48 +0900

    ABC

---
+++ Cpp/ABC/087/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    int x, a, b;
+    cin >> x >> a >> b;
+    cout << (x-a) % b << endl;
+    return 0;
+}
+

f3142b8c29d3f153bd1eb619395b340b85b9f9f7
nocotan <noconoco.lib@gmail.com>
2018-04-08 12:39:43 +0900

    ABC

---
+++ Cpp/ABC/090/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    string c1, c2, c3;
+    cin >> c1 >> c2 >> c3;
+    cout << c1[0] << c2[1] << c3[2] << endl;
+    return 0;
+}
+

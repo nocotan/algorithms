e881d61b58d49d464f4698cb43713c0b8214fe8e
nocotan <noconoco.lib@gmail.com>
2018-04-09 10:56:56 +0900

    ABC

---
+++ Cpp/ABC/091/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a+b>=c) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
+

1915f344652d10904436bfa1aaa5685a70e20371
nocotan <noconoco.lib@gmail.com>
2018-04-14 02:03:08 +0900

    arc

--- Cpp/ARC/090/C.cpp
+++ Cpp/ARC/090/C.cpp
@@ -1,22 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int A[2][101];
-int n;
+#define int long long
 
-int solve(int x, int y, int c) {
-    if(x>=n&&y>=2) return c;
-    if(y>=2 && x>=n) return max(solve(x+1, y, c+A[y][x+1]), solve(x, y+1, c+A[y+1][x]));
-    else if(x>=n) return solve(x, y+1, c+A[y+1][x]);
-    else return solve(x+1, y, c+A[y][x+1]);
-}
+int G[2][100];
+
+int dx[]{0, 1};
+int dy[]{1, 0};
 
 signed main() {
+    int n;
     cin >> n;
     for(int i=0; i<2; ++i) {
-        for(int j=0; j<n; ++j) cin >> A[i][j];
+        for(int j=0; j<n; ++j) cin >> G[i][j];
     }
-    cout << solve(0, 0, A[0][0]) << endl;
+
+    queue<pair<int, pair<int,int>>> que;
+    que.push(make_pair(G[0][0], make_pair(0, 0)));
+    int ans = -1;
+    while(!que.empty()) {
+        auto p = que.front(); que.pop();
+        int ame = p.first;
+        int x = p.second.first;
+        int y = p.second.second;
+        if(y==n-1&&x==1) {
+            ans = max(ame, ans);
+            continue;
+        }
+        for(int i=0; i<2; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx<2&&ny<n)
+                que.push(make_pair(ame+G[nx][ny], make_pair(nx, ny)));
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
 

1f16b28d29a060db0b42c8bf3b32f60fd66fed2a
Masanari Kimura <noconoco.lib@gmail.com>
2020-03-18 09:47:10 +0900

    add 1342

---
+++ leetcode/algorithms/1342.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+class Solution {
+    public:
+        int numberOfSteps(int num) {
+            int ans = 0;
+            while (num > 0) {
+                if (num % 2 ==0) num /= 2;
+                else num -= 1;
+                ++ans;
+            }
+
+            return ans;
+        }
+};
